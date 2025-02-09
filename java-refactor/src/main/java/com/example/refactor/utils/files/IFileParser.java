fc4a4fed3865364fa6e6f12c7ecdb4daa11b4f6f
Gianfranco <gfperez26@outlook.com>
2025-02-09 18:20:03 -0500

    Remove unused SongProcessor and SpotifyPlaylistJsonService classes along with related file parser interfaces

--- java-refactor/src/main/java/com/example/refactor/utils/files/IFileParser.java
+++
@@ -1,9 +0,0 @@
-package com.example.refactor.utils.files;
-
-import java.io.File;
-
-import com.example.refactor.domain.PlayList;
-
-public interface IFileParser {
-    public PlayList extracPlayList(File inputSource);
-}
